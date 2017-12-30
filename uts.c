071bf06ed04d9b0db9b4d2be5a32b0d62ccbc2ec
chris <lc1990linux@gmail.com>
2017-12-30 15:42:01 +0800

    add IPC separate

--- uts.c
+++ uts.c
@@ -15,13 +15,17 @@ char* const child_args[] = {
 
 int child_main(void* args) {
   printf("在子进程中\n");
+  //  sethostname("NewNamespace", 12);
   execv(child_args[0], child_args);
   return 1;
 }
 
 int main() {
   printf("程序开始：\n");
-  pid_t child_pid = clone(child_main, child_stack + STACK_SIZE, SIGCHLD, NULL);
+  int child_pid = clone(child_main, child_stack + STACK_SIZE, SIGCHLD, NULL);
+  /* int child_pid = clone(child_main, child_stack + STACK_SIZE, CLONE_NEWIPC | SIGCHLD, NULL); */
+  /* we can not set CLONE_NEWPIC it will return -1*/
+  printf("child_pid: %d \n", child_pid);
   waitpid(child_pid, NULL, 0);
   printf("己退出\n");
   return 0;
